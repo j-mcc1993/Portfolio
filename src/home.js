0954368dffefce1dfc9889a172d367023e69ea5d
j-mcc1993 <j.mcc3093@gmail.com>
2020-06-25 23:34:58 -0700

    Added project links/images, added overlay and hovering behaviors

--- src/home.js
+++ src/home.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import './images/me.jpg';
+import me from './images/me.jpg';
 
 /* Main profile picture component */
 function ProfilePic(props) {
     return (
-        <img className="profile-pic mx-auto d-block rounded-circle" src={"./images/me.jpg"} />
+        <img className="profile-pic mx-auto d-block rounded-circle" src={me} />
+    );
+}
+
+/* Project item picture */
+function Project(props) {
+    return (
+        <a class="project-link" href={props.url}>
+            {props.title}
+            <div class="project" style={props.imgStyle}>
+                <div class="project-overlay">
+                    <p class="overlay-par">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </div>
+            </div>
+        </a>
     );
 }
 
@@ -18,16 +34,16 @@ function NavBar(props) {
             </div>
             <div class="col-sm-4 nav-div">
                 <div class="nav-item-div">
-                    <p class="nav-item nav-item-active">Projects</p>
+                    <a href="#" class="nav-item nav-item-active">Projects</a>
                 </div>
                 <div class="nav-item-div">
-                    <p class="nav-item">Resume</p>
+                    <a href="#" class="nav-item">Resume</a>
                 </div>
                 <div class="nav-item-div">
-                    <p class="nav-item">Contact</p>
+                    <a href="#" class="nav-item">Contact</a>
                 </div>
                 <div class="nav-item-div">
-                    <p class="nav-item">About Me</p>
+                    <a href="#" class="nav-item">About Me</a>
                 </div>
             </div>
         </div>
@@ -44,17 +60,17 @@ function JumboTron(props) {
                         <ProfilePic />
                     </div>
                     <div class="namecard-tag">
-                        <h2 class="namecard-header"><span>Hello, </span>I'm James</h2>
+                        <h2 class="namecard-header"><span class="orange">Hello! </span>I'm James</h2>
                         <div class="namecard-par-div">
                             <p class="namecard-par">
                                 I am a programmer, web developer, musician and
                                 photographer living in the San Francisco Bay Area.
-                                </p>
+                            </p>
                         </div>
                     </div>
                 </div>
                 <div class="hire-me-header">
-                    <h2>Want to <span>hire me</span>?</h2>
+                    <h2>Want to <span class="blue">hire me</span>?</h2>
                     <p class="hire-me-par">Email me at: <address><b>j1mccull@ucsd.edu</b></address></p>
                 </div>
             </div >
@@ -69,16 +85,24 @@ function MainBody(props) {
             <h1 class="page-header">Projects</h1>
             <div class="project-container">
                 <div class="project-wrapper">
-                    <div class="project"></div>
-                    <div class="project"></div>
-                    <div class="project"></div>
-                    <div class="project"></div>
-                    <div class="project"></div>
-                    <div class="project"></div>
+                    <Project url={'https://github.com/j-mcc1993/Portfolio'}
+                        imgStyle={{ backgroundImage: "url('../images/port.jpg')" }}
+                        title={'Portfolio Website'} />
+                    <Project url={'https://github.com/j-mcc1993/NES_Keyboard'}
+                        imgStyle={{ backgroundImage: "url('../images/nes.jpg')" }}
+                        title={'NES_Keyboard Arduino Library'} />
+                    <Project url={'https://github.com/j-mcc1993/SNES_Keyboard'}
+                        imgStyle={{ backgroundImage: "url('../images/snes.jpg')" }}
+                        title={'SNES_Keyboard Arduino Library'} />
+                    <Project url={'https://gist.github.com/j-mcc1993/694917ca74b254b56eef4560ff0d9eea'}
+                        imgStyle={{ backgroundImage: "url('../images/code.jpg')" }}
+                        title={'Reddit Meme-Posting Bot'} />
+                    <Project url={'https://gist.github.com/j-mcc1993/ef75a9227eeac139ee94'}
+                        imgStyle={{ backgroundImage: "url('../images/fractal.png')" }}
+                        title={'Bitwise Fractal Image Generator'} />
                 </div>
             </div>
         </div>
-
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,19 +3,24 @@ import ReactDOM from 'react-dom';
 import './styles/stylesheet.css';
 import TopLvlBody from './home.js';
 import WebFont from 'webfontloader';
+import './images/snes.jpg';
+import './images/nes.jpg';
+import './images/port.jpg';
+import './images/code.jpg';
+import './images/fractal.png';
 
-  WebFont.load({
-
+WebFont.load({
     google: {
-      families: [
-                'Shadows Into Light',
-                'Yellowtail', 
-                'Roboto', 
-                'Pacifico', 
-                'Permanent Marker', 
-                'Baloo Thambi 2:800'
-                ]
+        families: [
+            'Shadows Into Light',
+            'Yellowtail',
+            'Rock Salt',
+            'Pacifico',
+            'Permanent Marker',
+            'Baloo Thambi 2:800'
+        ]
 
-     }});
+    }
+});
 
 ReactDOM.render(<TopLvlBody />, document.getElementById("root"));
